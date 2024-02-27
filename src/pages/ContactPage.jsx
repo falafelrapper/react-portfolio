2432756bfd0563e70ba32b41ff91044002728b45
Zachary Roy <vgd199@gmail.com>
2024-02-26 19:03:52 -0800

    added some changes to fades

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -52,11 +52,11 @@ export default function ContactPage() {
       <div className="contact-window">
         <div className="contact-card">
           <h2>Contact Me</h2>
-          <Link to='https://www.linkedin.com/in/zachary-maxwell-roy/' target="_blank" rel="noopener noreferrer"><img id='linkedin' src="../images/linkedin.png" alt="linkedIn" /></Link>
+          <Link to='https://www.linkedin.com/in/zachary-maxwell-roy/' target="_blank" rel="noopener noreferrer"><img id='linkedin' src="/images/linkedin.png" alt="linkedIn" /></Link>
           <p>LinkedIn</p>
-          <Link to="mailto:zacharymaxwellroy@gmail.com"><img id='email' src="../images/email.png" alt="Email" /></Link>
+          <Link to="mailto:zacharymaxwellroy@gmail.com"><img id='email' src="/images/email.png" alt="Email" /></Link>
           <p>zacharymaxwellroy@gmail.com</p>
-          <Link to="tel:+3237173790"><img id='phone' src="../images/phone.png" alt="Phone" /></Link>
+          <Link to="tel:+3237173790"><img id='phone' src="/images/phone.png" alt="Phone" /></Link>
           <p>323.717.3790</p>
         </div>
 
